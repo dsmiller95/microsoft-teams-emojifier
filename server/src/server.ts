012689b5e80bfe0d8d6b4982bdc45c9d59534d9f
Dan Miller <dan.miller@centare.com>
2019-07-28 17:49:06 -0500

    moved client back up to top. creating new repo for server

--- server/src/server.ts
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-
-const app = express();
-
-app.get('/', (req, res) => res.send('Hello Emoji Users!!'));
-
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
